a2ad443e982e380d99e4a458f7e55c421fa9f54a
Rahul <rahulmore1113@gmail.com>
2023-11-11 06:53:18 +0530

    Sec

--- SPRING-GITHUB-LOGIN/src/main/java/com/rahul/SpringGithubLoginApplication.java
+++ SPRING-GITHUB-LOGIN/src/main/java/com/rahul/SpringGithubLoginApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringGithubLoginApplication {
+public class SpringGithubLoginApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(SpringGithubLoginApplication.class, args);
 	}
 
